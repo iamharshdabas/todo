a09726942a030e1e72c85b30b1149623e7549b86
Harsh Dabas <iamharshdabas@gmail.com>
2023-05-16 19:45:11 +0530

    ADD Todo.js: delete button

--- src/comp/Todo.js
+++ src/comp/Todo.js
@@ -2,7 +2,12 @@ export default function Header({ todos }) {
   return (
     <>
       {todos.map((todo) => {
-        return <h1>{todo.item}</h1>
+        return (
+          <>
+            <h1>{todo.item}</h1>
+            <button className="btn btn-danger btn-sm">Delete</button>
+          </>
+        )
       })}
     </>
   )
