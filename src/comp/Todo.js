20e03b7981a0d28a23e5ae1e4c070cfe0bd641f8
Harsh Dabas <iamharshdabas@gmail.com>
2023-05-17 16:02:06 +0530

    ADD Todo.js: no todo message

--- src/comp/Todo.js
+++ src/comp/Todo.js
@@ -1,14 +1,16 @@
 export default function Todo({ todos, onDelete }) {
   return (
     <>
-      {todos.map((todo) => {
-        return (
-          <>
-            <h1>{todo.item}</h1>
-            <button className="btn btn-danger btn-sm" onClick={() => onDelete(todo)}>Delete</button>
-          </>
-        )
-      })}
+      {todos.length === 0 ? 'No Todos' :
+        todos.map((todo) => {
+          return (
+            <>
+              <h1>{todo.item}</h1>
+              <button className="btn btn-danger btn-sm" onClick={() => onDelete(todo)}>Delete</button>
+            </>
+          )
+        })
+      }
     </>
   )
 }
