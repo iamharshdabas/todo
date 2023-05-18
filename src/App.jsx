071ffcee606becaaa2e5de46b2744e51f88004af
Harsh Dabas <iamharshdabas@gmail.com>
2023-05-18 12:48:26 +0530

    ADD: working todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,47 @@
+import { useState } from "react"
+
 function App() {
+  const [newTodo, setNewTodo] = useState('')
+  const [todos, setTodos] = useState([])
+
+  const onDelete = (todo) => {
+    setTodos(todos.filter((item) => {
+      return item !== todo
+    }))
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    setTodos((todos) => {
+      return [
+        ...todos,
+        { message: newTodo }
+      ]
+    })
+  }
+
   return (
     <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="newTodoInput">
+          New Todo:
+          <input
+            type="text"
+            name="newTodoInput"
+            value={newTodo}
+            onChange={(event) => setNewTodo(event.target.value)}
+            id="newTodoInput" />
+        </label>
+        <button type="submit">Add</button>
+      </form>
+      {todos.map((todo) => {
+        return (
+          <>
+            <h1>{todo.message}</h1>
+            <button type="button" onClick={() => onDelete(todo)}>Delete</button>
+          </>
+        )
+      })}
     </>
   )
 }
